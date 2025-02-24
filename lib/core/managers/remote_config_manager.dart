26a80365e636e7a0c59d932a66851a1c0dcf0159
ugochukwu <dykeehenry@gmail.com>
2025-02-24 12:11:45 +0100

    Remove unused ad-related remote config parameters

--- lib/core/managers/remote_config_manager.dart
+++ lib/core/managers/remote_config_manager.dart
@@ -9,12 +9,6 @@ class RemoteConfigManager {
   FirebaseRemoteConfig? remoteConfig;
 
   String get requiredVersion => remoteConfig!.getString(_getRemoteConfigName("required_version"));
-  String get ad1_url => remoteConfig!.getString(_getRemoteConfigName("ad1_url"));
-  String get ad2_url => remoteConfig!.getString(_getRemoteConfigName("ad2_url"));
-  bool get enable_ad1 => remoteConfig!.getBool(_getRemoteConfigName("enable_ad1"));
-  bool get enable_ad2 => remoteConfig!.getBool(_getRemoteConfigName("enable_ad2"));
-  String get ad1_link => remoteConfig!.getString(_getRemoteConfigName("ad1_link"));
-  String get ad2_link => remoteConfig!.getString(_getRemoteConfigName("ad2_link"));
 
 
   RemoteConfigManager._internal() {
